a228c0d9256a35eb69f34f09d019133c8932abd5
kamcho19 <kamcho19@naver.com>
2018-03-30 20:28:20 +0200

    Set Mybatis
    set ORACLE DB
    added context-datasource.xml(set oracle info)
    added context-mapper.xml
    added AbstractDAO.java
    스프링(Spring) 개발 - (9) Mybatis (마이바티스) 연동하기
    출처: http://addio3305.tistory.com/62?category=772645 [흔한 개발자의 개발 노트]

--- first/src/main/java/first/sample/controller/SampleController.java
+++ first/src/main/java/first/sample/controller/SampleController.java
@@ -14,7 +14,7 @@ public class SampleController {
 	@RequestMapping(value="/sample/openSampleList.do")
 	public ModelAndView openSampleList(Map<String, Object> commandMap) throws Exception{
 		ModelAndView mv =new ModelAndView("");
-		log.debug("인터셉트 테스트/::::::");
+		log.debug("인터셉트 테스트");
 		
 		return mv;
 	}
