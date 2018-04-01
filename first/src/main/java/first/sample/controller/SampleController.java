44fc9573ebea30e7d1bdfff4281195ee7a1a532b
kamcho19 <kamcho19@naver.com>
2018-04-01 03:08:05 +0200

    Adapt HandlerMethodArgumentResolver
    Added CommandMap.java
    Added CustomMapArgumentResolver.java
    
    스프링(Spring) 개발 - (11) HandlerMethodArgumentResolver 적용
    출처: http://addio3305.tistory.com/75?category=772645 [흔한 개발자의 개발 노트]

--- first/src/main/java/first/sample/controller/SampleController.java
+++ first/src/main/java/first/sample/controller/SampleController.java
@@ -1,7 +1,9 @@
 package first.sample.controller;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.annotation.Resource;
 
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import first.common.common.CommandMap;
 import first.sample.service.SampleService;
 
 @Controller
@@ -34,6 +37,21 @@ public class SampleController {
 		List<Map<String, Object>> list = sampleService.selectBoardList(commandMap);
 		mv.addObject("list", list);
 		
+		return mv;
+	}
+	
+	@RequestMapping(value="/sample/testMapArgumentResolver.do")
+	public ModelAndView testMapArgumentResolver(CommandMap commandMap) throws Exception{
+		ModelAndView mv = new ModelAndView("");
+		
+		if(commandMap.isEmpty() == false) {
+			Iterator<Entry<String, Object>> iterator = commandMap.getMap().entrySet().iterator();
+			Entry<String, Object> entry = null;
+			while(iterator.hasNext()) {
+				entry = iterator.next();
+				log.debug("key : "+entry.getKey()+", value : "+entry.getValue());
+			}
+		}
 		return mv;
 	}
 
